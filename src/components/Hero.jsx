cc9629b2d9af3a5ed0c72c8eab2e88b12a21482f
Seyfadin Abdela <seyfadinabdela42@gmail.com>
2024-12-01 11:29:32 -0800

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/Hero.css";
-
+import { Link } from "react-router-dom";
 const Hero = () => {
   return (
     <div className="hero">
@@ -12,8 +12,12 @@ const Hero = () => {
             Dive into a world of chills and thrills. Watch the most terrifying and exciting horror movies!
           </p>
           <div className="action-buttons">
-   <a href="/movies"><button className="action-button primary">Browse Movies</button></a>
-    <a href="/movies"><button className="action-button secondary">Watch Trailer</button></a>
+      <Link to="/movies">
+              <button className="action-button primary">Browse Movies</button>
+            </Link>
+            <Link to="/movies">
+              <button className="action-button secondary">Watch Trailer</button>
+            </Link>
   </div>
         </div>
       </div>
