f8601cc17751f5185235b5a61b2866e57e5e9769
Seyfadin Abdela <seyfadinabdela42@gmail.com>
2024-12-01 11:20:11 -0800

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,14 +6,14 @@ const Hero = () => {
     <div className="hero">
       <div className="hero-overlay">
         <div className="hero-content">
-          <h1 className="hero-title">Welcome to ScaryFlix</h1><br /><br />
+          <h1 className="hero-title">Welcome to ScaryFlix</h1><br />
           {/* <h4 className="hero-title">Are Youlooking for Immersive Movies!?</h4> */}
           <p className="hero-subtitle">
             Dive into a world of chills and thrills. Watch the most terrifying and exciting horror movies!
-          </p><br />
+          </p>
           <div className="action-buttons">
-    <button className="action-button primary">Browse Movies</button>
-    <button className="action-button secondary">Watch Trailer</button>
+   <a href="/movies"><button className="action-button primary">Browse Movies</button></a>
+    <a href="/movies"><button className="action-button secondary">Watch Trailer</button></a>
   </div>
         </div>
       </div>
